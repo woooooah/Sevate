127958a79f0d9d7d4e52f92b234faa8f363507a9
Kaja <riposte_pins0t@icloud.com>
2026-03-27 20:52:36 +0100

    404 stran - 1. test

---
+++ themes/hello-elementor-child/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div style="text-align: center; padding: 80px 20px;">
+    <h1>Ups, ta stran ne obstaja!</h1>
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" style="
+        display: inline-block;
+        margin-top: 20px;
+        padding: 12px 28px;
+        background-color: #415A77;
+        color: #ffffff;
+        text-decoration: none;
+        border-radius: 4px;
+        font-size: 16px;
+    ">
+        Domača stran
+    </a>
+</div>
+
+<?php get_footer(); ?>
